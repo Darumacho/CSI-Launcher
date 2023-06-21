b71dfc4b50c2c6157da7ff37d46435d1ba0608cf
Adrien L <adri1.lepinay@free.fr>
2023-06-21 18:28:25 +0200

    Son startup

--- GameLauncher/Main.xaml.cs
+++ GameLauncher/Main.xaml.cs
@@ -25,15 +25,14 @@ namespace GameLauncher
         private void ButtonClicked_1(object sender, RoutedEventArgs e)
         {
             CSIWindow csi1 = new CSIWindow();
-            //SoundPlayer start = new SoundPlayer("sound/Startup.wav");
-            //start.Load();
-            //start.Play();
+            this.SoundClick();
             csi1.Show();
             this.Close();
         }
         private void ButtonClicked_2(object sender, RoutedEventArgs e)
         {
             CSIIWindow csi2 = new CSIIWindow();
+            this.SoundClick();
             csi2.Show();
             this.Close();
         }
@@ -41,8 +40,18 @@ namespace GameLauncher
         private void ButtonClicked_R(object sender, RoutedEventArgs e)
         {
             CSRWindow csir = new CSRWindow();
+            this.SoundClick();
             csir.Show();
             this.Close();
+        }
+
+        private void SoundClick()
+        {
+            //SoundPlayer start = new SoundPlayer("C:/Users/Darumacho/Documents/Visual Studio 2019/MultiLauncher/GameLauncher/sound/Startup.wav");
+            string Audio_FilePath = AppDomain.CurrentDomain.BaseDirectory + "sound\\Startup.wav";
+            SoundPlayer start = new SoundPlayer(Audio_FilePath);
+            start.Load();
+            start.Play();
         }
     }
 }
